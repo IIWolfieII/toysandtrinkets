95f3f2acdbe56fd9126789313eb55005cd00226e
IIWolfieII <iiwolfieii@yahoo.com>
2022-07-26 18:08:51 +0100

    Default Push

---
+++ src/main/java/com/iiwolfieii/toysandtrinkets/ToysAndTrinkets.java
@@ -0,0 +1,17 @@
+package com.iiwolfieii.toysandtrinkets;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ToysAndTrinkets implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("toysandtrinkets");
+
+    @Override
+    public void onInitialize() {
+
+
+        LOGGER.info("HELLO EVERYONEEEEE!");
+    }
+}
---
+++ src/main/java/com/iiwolfieii/toysandtrinkets/item/ModItems.java
@@ -0,0 +1,4 @@
+package com.iiwolfieii.toysandtrinkets.item;
+
+public class ModItems {
+}
